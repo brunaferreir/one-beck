f413cae84c484cb8cb0010405a46c520f99f96bc
bruna ferreira <brunaferreira0320@gmail.com>
2023-03-29 11:15:13 -0300

    estilo

---
+++ node_7da37c72f4a59.tmp.js
@@ -0,0 +1,100 @@
+*{
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+}
+
+/* Mini Framework */
+
+html,body{
+    height: 100%;
+}
+.max-widht{
+    width: 100%;
+    min-height: 100vh;
+}
+
+.bg{
+    background:  url('image/bg.png') no-repeat;
+    background-size: cover;
+    height: auto;
+}
+
+.container{
+    max-width: 1330px;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+}
+
+/* Tamanho de Fontes */
+
+.text-gd{
+    font-size:  64px;
+
+}
+
+.text-md{
+    font-size: 36px;
+}
+
+.text-pq{
+    font-size: 14px;
+}
+
+.text-title{
+    font-size: 20px;
+    font-weight: 600;
+}
+
+/*  Corde de texto */
+
+.color-verde{
+    color: #275C29;
+}
+
+.cor-cinza-1{
+    color: #2b2b2b;
+}
+.cor-cinza-2{
+    color: #545454 ;
+}
+
+/* Chamada */
+
+.call{
+    display: flex;
+    align-items: center;
+    margin-top: 120px;
+}
+
+.left{
+    width: 50%;
+    min-height: 600px;
+    max-height: 600px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 2%;
+}
+
+.right{
+    width: 50%;
+    min-height: 600px;
+    max-height: 600px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 2%;
+}
+
+/* Imagem */
+
+img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+/*botã
